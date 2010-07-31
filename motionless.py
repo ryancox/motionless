35dcc405c8e84831b71ffd204a1b70c3aed46445
Ryan Cox <ryan.a.cox@gmail.com>
2010-07-30 20:39:00 -0400

    map types added

--- motionless.py
+++ motionless.py
@@ -4,15 +4,8 @@ from urllib import quote
 
 """
     TODO:
-        include sets of valid maptypes,sizes,...
-        include factories or new types: centerMap, markerMap, visibleMap,pathMap
-        image format
-        maptype
         paths 
         encoded polylines: http://code.google.com/p/py-gpolyencode/
-        visible / viewports
-        shortening urls for markers: http://github.com/bitly/bitly-api-python
-        circle / radus drawing: http://code.google.com/p/geopy/
 """
 
 class Marker:
@@ -35,14 +28,14 @@ class LatLonMarker(Marker):
 
 class Map:
     def __init__(self):
-        self.base_url = "http://maps.google.com/maps/api/staticmap?"
+        self.base_url = 'http://maps.google.com/maps/api/staticmap?'
         self.size_x = 400
         self.size_y = 400
-        self.center = quote("1600 Amphitheatre Parkway Mountain View, CA")
         self.sensor = False
-        self.zoom = 16
-        self.format = "png"
-        self.markers = []
+        self.format = 'png'
+        self.FORMATS = ['png','png8','png32','gif','jpg','jpg-baseline']
+        self.maptype = 'roadmap' 
+        self.MAPTYPES = ['roadmap','satellite','hybrid','terrain']
 
     def check_parameters(self):
         pass
@@ -53,16 +46,43 @@ class Map:
         else:
             return 'false'
 
+    def download(self, filename): 
+        o = urllib2.urlopen(self.generate_url())
+        print o.headers
+        open(filename,'wb').write(o.read())
+    
+
+class CenterMap(Map):
+    def __init__(self, address=None):
+        Map.__init__(self)                    
+        if address: 
+            self.center = quote(address)
+        else:
+            self.center = "1600 Amphitheatre Parkway Mountain View, CA"
+        self.zoom = 14
+
+    def set_address(self,address):
+        self.center = quote(address)
+
+    def set_lat_lon(self,lat,lon):
+        self.center = "%s,%s" % (quote(lat),quote(lon))
+
     def generate_url(self):
         self.check_parameters();
-        return "%scenter=%s&zoom=%s&size=%sx%s&sensor=%s&%s&" % (
+        return "%smaptype=%s&format=%s&center=%s&zoom=%s&size=%sx%s&sensor=%s" % (
             self.base_url,
+            self.maptype,
+            self.format,
             self.center,
             self.zoom,
             self.size_x,
             self.size_y,
-            self._get_sensor(),
-            self._generate_markers())
+            self._get_sensor())
+
+class MarkerMap(Map):
+    def __init__(self):
+        Map.__init__(self)                    
+        self.markers = []
 
     def _generate_markers(self):
         styles = set()
@@ -80,7 +100,7 @@ class Map:
                 data[(marker.size,marker.color,marker.label)].append(quote(marker.address))
             if isinstance(marker,LatLonMarker):
                 location = "%s,%s" % (marker.latitude, marker.longitude)
-                data[(marker.size,marker.color,marker.label)].append(location)
+                data[(marker.size,marker.color,marker.label)].append(quote(location))
         # build markers entries for URL 
         for style in data:
             locations = data[style]
@@ -97,12 +117,40 @@ class Map:
             ret.append("|".join(parts))
         return "&".join(ret) 
 
-    def download(self, filename): 
-        o = urllib2.urlopen(self.generate_url())
-        print o.headers
-        open(filename,'wb').write(o.read())
-    
     def add_marker(self, marker):
         if not isinstance(marker,Marker):
             raise  ValueError("Must pass instance of Marker to add_marker")
         self.markers.append(marker)     
+
+    def generate_url(self):
+        self.check_parameters();
+        return "%smaptype=%s&format=%s&size=%sx%s&sensor=%s&%s&" % (
+            self.base_url,
+            self.maptype,
+            self.format,
+            self.size_x,
+            self.size_y,
+            self._get_sensor(),
+            self._generate_markers())
+
+class VisibleMap(Map):
+    def __init__(self):
+        Map.__init__(self)                    
+        self.locations = []
+
+    def add_address(self, address):
+        self.locations.append(quote(address))
+
+    def add_lat_lon(self, lat, lon):
+        self.locations.append("%s,%s" % (quote(lat),quote(lon)))
+
+    def generate_url(self):
+        self.check_parameters();
+        return "%smaptype=%s&format=%s&size=%sx%s&sensor=%s&visible=%s" % (
+            self.base_url,
+            self.maptype,
+            self.format,
+            self.size_x,
+            self.size_y,
+            self._get_sensor(),
+            "|".join(self.locations))
