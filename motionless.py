3c7742ac1779b46916755e1ce99685f2e94ef5f2
Ryan Cox <ryan.a.cox@gmail.com>
2010-08-01 14:43:32 -0400

    refactor classes

--- motionless.py
+++ motionless.py
@@ -4,42 +4,67 @@ from urllib import quote
 
 """
     TODO:
-        implement param checks
-        paths 
-        encoded polylines: http://code.google.com/p/py-gpolyencode/
+        confirm color format / opacity    
+        properly handle
+            path weight,color,opacity,fillcolor
+
+        change gpx to airport
+        handle 414 - URI too long
 """
 
-class Marker:
-    pass
+class Marker(object):
+    SIZES =  ['tiny','mid','small']
+    COLORS = ['black', 'brown', 'green', 'purple', 'yellow', 'blue', 'gray', 'orange', 'red', 'white']
+    LABELS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    def __init__(self, size, color, label):
+        if size and size not in Marker.SIZES:
+            raise ValueError("[%s] is not a valid marker size. Valid sizes include %s" % (size,Marker.SIZES))
+        if label and (len(label) <> 1 or not label in Marker.LABELS):
+            raise ValueError("[%s] is not a valid label. Valid labels are a single character 'A'..'Z' or '0'..'9'"  % label)
+        if color and color not in Marker.COLORS:
+            raise ValueError("[%s] is not a valid color. Valid colors include %s"  % ( color, Marker.COLORS ))
+        self.size = size
+        self.color = color
+        self.label = label
 
 class AddressMarker(Marker):
     def __init__(self, address, size=None, color=None, label=None):
+        Marker.__init__(self,size,color,label)
         self.address = address
-        self.size = size
-        self.color = color
-        self.label = label
 
 class LatLonMarker(Marker):
     def __init__(self, lat, lon, size=None, color=None, label=None):
+        Marker.__init__(self,size,color,label)
         self.latitude = lat
         self.longitude = lon
-        self.size = size
-        self.color = color
-        self.label = label
 
-class Map:
-    def __init__(self):
+class Map(object):
+    MAX_URL_LEN = 2048
+    MAPTYPES = ['roadmap','satellite','hybrid','terrain']
+    FORMATS = ['png','png8','png32','gif','jpg','jpg-baseline']
+    MAX_X = 640
+    MAX_Y = 640
+    def __init__(self, size_x, size_y, maptype):
+
         self.base_url = 'http://maps.google.com/maps/api/staticmap?'
-        self.size_x = 400
-        self.size_y = 400
-        self.sensor = False
+        self.size_x = size_x 
+        self.size_y = size_y 
+        self.sensor = False 
         self.format = 'png'
-        self.FORMATS = ['png','png8','png32','gif','jpg','jpg-baseline']
-        self.maptype = 'roadmap' 
-        self.MAPTYPES = ['roadmap','satellite','hybrid','terrain']
+        self.maptype = maptype 
 
     def check_parameters(self):
-        pass
+        if self.format not in Map.FORMATS:
+            raise ValueError("[%s] is not a valid file format. Valid formats include %s" % (self.format,Map.FORMATS))
+    
+        if self.format not in Map.FORMATS:
+            raise ValueError("[%s] is not a valid map type. Valid types include %s" % (self.maptype,Map.MAPTYPES))
+        
+        if self.size_x > Map.MAX_X or self.size_x < 1:
+            raise ValueError("[%s] is not a valid x-dimension. Must be between 1 and %s" % (self.size_x,Map.MAX_X))
+
+        if self.size_y > Map.MAX_Y or self.size_y < 1:
+            raise ValueError("[%s] is not a valid y-dimension. Must be between 1 and %s" % (self.size_x,Map.MAX_Y))
 
     def _get_sensor(self):
         if self.sensor:
@@ -47,30 +72,31 @@ class Map:
         else:
             return 'false'
 
-    def download(self, filename): 
-        o = urllib2.urlopen(self.generate_url())
-        print o.headers
-        open(filename,'wb').write(o.read())
-    
+    def _check_url(self, url):
+        if len(url) > Map.MAX_URL_LEN:
+            raise ValueError("Generated URL is %s characters in length. Maximum is %s" % (len(url),Map.MAX_URL_LEN))
 
 class CenterMap(Map):
-    def __init__(self, address=None):
-        Map.__init__(self)                    
+    ZOOM_RANGE = range(1,21)
+    def __init__(self, address=None, lat=None, lon=None, zoom=14, size_x=400, size_y=400, maptype='roadmap'):
+        Map.__init__(self,size_x=size_x, size_y=size_y, maptype=maptype)                    
         if address: 
             self.center = quote(address)
+        elif lat and lon:
+            self.center = "%s,%s" % (lat,lon)
         else:
             self.center = "1600 Amphitheatre Parkway Mountain View, CA"
-        self.zoom = 14
+        self.zoom = zoom 
 
-    def set_address(self,address):
-        self.center = quote(address)
-
-    def set_lat_lon(self,lat,lon):
-        self.center = "%s,%s" % (lat,lon)
+    def check_parameters(self):
+        super(CenterMap,self).check_parameters()
+        
+        if self.zoom not in CenterMap.ZOOM_RANGE:
+            raise ValueError("[%s] is not a zoom setting. Must be between %s and %s" % (self.size_x,min(CenterMap.ZOOM_RANGE),max(CenterMap.ZOOM_RANGE)))
 
     def generate_url(self):
         self.check_parameters();
-        return "%smaptype=%s&format=%s&center=%s&zoom=%s&size=%sx%s&sensor=%s" % (
+        url = "%smaptype=%s&format=%s&center=%s&zoom=%s&size=%sx%s&sensor=%s" % (
             self.base_url,
             self.maptype,
             self.format,
@@ -79,11 +105,55 @@ class CenterMap(Map):
             self.size_x,
             self.size_y,
             self._get_sensor())
+    
+        self._check_url(url)
+        return url
 
-class MarkerMap(Map):
-    def __init__(self):
-        Map.__init__(self)                    
+class VisibleMap(Map):
+    def __init__(self, size_x=400, size_y=400, maptype='roadmap'):
+        Map.__init__(self,size_x=size_x, size_y=size_y, maptype=maptype)                    
+        self.locations = []
+
+    def add_address(self, address):
+        self.locations.append(quote(address))
+
+    def add_latlon(self, lat, lon):
+        self.locations.append("%s,%s" % (quote(lat),quote(lon)))
+
+    def generate_url(self):
+        self.check_parameters();
+        url = "%smaptype=%s&format=%s&size=%sx%s&sensor=%s&visible=%s" % (
+            self.base_url,
+            self.maptype,
+            self.format,
+            self.size_x,
+            self.size_y,
+            self._get_sensor(),
+            "|".join(self.locations))
+
+        self._check_url(url)
+        return url
+
+class DecoratedMap(Map):
+    def __init__(self, size_x=400, size_y=400,maptype='roadmap',region=False,fillcolor='green'):
+        Map.__init__(self,size_x=size_x, size_y=size_y,maptype=maptype)                    
         self.markers = []
+        self.fillcolor = fillcolor
+        self.region = region
+        self.path = []
+        self.contains_addresses = False
+
+    def check_parameters(self):
+        super(DecoratedMap,self).check_parameters()
+        
+        if self.region and len(self.path) < 2: 
+            raise ValueError("At least two path elements required if region is enabled")
+
+        if self.region and self.path[0] <> self.path[-1]: 
+            raise ValueError("If region enabled, first and last path entry must be identical")
+
+        if len(self.path) == 0 and len(self.markers) == 0:
+            raise ValueError("Must specify points in path or markers")
 
     def _generate_markers(self):
         styles = set()
@@ -118,65 +188,53 @@ class MarkerMap(Map):
             ret.append("|".join(parts))
         return "&".join(ret) 
 
+    def _can_polyencode(self):
+        try:
+            import gpolyencode 
+        except:
+            return False
+        return not self.contains_addresses    
+    
+    def _polyencode(self):
+        import gpolyencode 
+        encoder = gpolyencode.GPolyEncoder()
+        points = []
+        for point in self.path:
+            tokens = point.split(',')
+            points.append((float(tokens[1]),float(tokens[0])))
+        return encoder.encode(points)['points']
+
     def add_marker(self, marker):
         if not isinstance(marker,Marker):
-            raise  ValueError("Must pass instance of Marker to add_marker")
+            raise ValueError("Must pass instance of Marker to add_marker")
         self.markers.append(marker)     
 
+    def add_path_address(self, address):
+        self.contains_addresses = True
+        self.path.append(quote(address))
+
+    def add_path_latlon(self, lat, lon):
+        self.path.append("%s,%s" % (quote(lat),quote(lon)))
+
     def generate_url(self):
         self.check_parameters();
-        return "%smaptype=%s&format=%s&size=%sx%s&sensor=%s&%s&" % (
+        url = "%smaptype=%s&format=%s&size=%sx%s&sensor=%s" % (
             self.base_url,
             self.maptype,
             self.format,
             self.size_x,
             self.size_y,
-            self._get_sensor(),
-            self._generate_markers())
+            self._get_sensor())
 
-class VisibleMap(Map):
-    def __init__(self):
-        Map.__init__(self)                    
-        self.locations = []
+        if len(self.markers) > 0:
+            url = "%s&%s" % ( url, self._generate_markers())
 
-    def add_address(self, address):
-        self.locations.append(quote(address))
+        if len(self.path) > 0:
+            if self._can_polyencode():
+                url = "%s&path=enc:%s" % ( url, self._polyencode())
+            else:
+                url = "%s&path=%s" % ( url, "|".join(self.path))
 
-    def add_lat_lon(self, lat, lon):
-        self.locations.append("%s,%s" % (quote(lat),quote(lon)))
+        self._check_url(url)
 
-    def generate_url(self):
-        self.check_parameters();
-        return "%smaptype=%s&format=%s&size=%sx%s&sensor=%s&visible=%s" % (
-            self.base_url,
-            self.maptype,
-            self.format,
-            self.size_x,
-            self.size_y,
-            self._get_sensor(),
-            "|".join(self.locations))
-
-class PathMap(Map):
-    def __init__(self):
-        Map.__init__(self)                    
-        self.locations = []
-
-    def add_address(self, address):
-        self.locations.append(quote(address))
-
-    def add_lat_lon(self, lat, lon):
-        self.locations.append("%s,%s" % (quote(lat),quote(lon)))
-
-    def generate_url(self):
-        self.check_parameters();
-        return "%smaptype=%s&format=%s&size=%sx%s&sensor=%s&path=%s" % (
-            self.base_url,
-            self.maptype,
-            self.format,
-            self.size_x,
-            self.size_y,
-            self._get_sensor(),
-            "|".join(self.locations))
-
-class RegionMap(Map):
-    pass
+        return url
