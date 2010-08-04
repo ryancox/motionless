8949a14bfc737d2f55ef48e68092264e5cd9e9f9
ryancox <ryan.a.cox@gmail.com>
2010-08-03 20:00:35 -0700

    setup and comments

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import sys, os
+
+__author__ = 'Ryan Cox <ryan.a.cox@gmail.com>'
+__version__ = '1.0'
+
+# Distutils version
+METADATA = dict(
+	name = "motionless",
+	version = __version__,
+	py_modules = ['setup', 'motionless'],
+	author = 'Ryan Cox',
+	author_email = 'ryan.a.cox@gmail.com',
+	description = 'An easy way to generate Google Static Map URLs with Python.',
+	long_description = open("README.md").read(),
+	license = 'Apache 2.0 License',
+	url = 'http://github.com/ryancox/motionless',
+	keywords = 'google static maps url api georss mapping gpx kml geo gis',
+)
+
+# Setuptools version
+SETUPTOOLS_METADATA = dict(
+	install_requires = ['setuptools'],
+	include_package_data = True,
+	classifiers = [
+		'Development Status :: 4 - Beta',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: Apache Public Source License',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+                'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+                'Topic :: Multimedia :: Graphics :: Presentation',
+		'Topic :: Internet',
+	]
+)
+
+def Main():
+	try:
+		import setuptools
+		METADATA.update(SETUPTOOLS_METADATA)
+		setuptools.setup(**METADATA)
+	except ImportError:
+		import distutils.core
+		distutils.core.setup(**METADATA)
+
+if __name__ == '__main__':
+  Main()
