8303e8a8a4f431e20682671c822007e5367993e8
Ryan Cox <ryan.a.cox@gmail.com>
2016-01-30 08:56:15 -0800

    revert out changes. did not see these were made in different branch already

--- examples/earthquakes.py
+++ examples/earthquakes.py
@@ -12,7 +12,7 @@ opener = request.urlopen('http://earthquake.usgs.gov/earthquakes/feed/v1.0'
                          '/summary/2.5_day.geojson')
 
 # Parse it
-gjs = geojson.loads(opener.read().decode('UTF-8'))
+gjs = geojson.loads(opener.readall().decode('utf-8'))
 
 # Prepare a map and add the points
 gmap = DecoratedMap(size_x=640,size_y=440)
@@ -34,7 +34,7 @@ htmlPage = """
 <html>
 <body>
 <h2>Earthquakes 2.5+ today</h2>
-<i>GeoJSON feed parsed and visualized. Colors assigned to markers based on
+<i>GeoRSS feed parsed and visualized. Colors assigned to markers based on
 magnitudes. Details can be found on the
 <a href='http://earthquake.usgs.gov/earthquakes/'>USGS web site</a>.
 <p/>
@@ -44,6 +44,6 @@ magnitudes. Details can be found on the
 </html>
 """ % gmap.generate_url()
 
-with open("earthquakes.html","w") as html:
+with open("earthquakes.html","w")as html:
     html.write(htmlPage)
 print("earthquakes.html file created")
