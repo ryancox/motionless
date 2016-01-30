b7e21b1c0a33d37884e851f5d5e30518fb97ac8f
Fabien Maussion <fabien.maussion@uibk.ac.at>
2016-01-30 16:18:19 +0100

    Fix earthquake example

--- examples/earthquakes.py
+++ examples/earthquakes.py
@@ -1,71 +1,49 @@
-from motionless import AddressMarker, LatLonMarker,DecoratedMap, CenterMap, VisibleMap
-import xml.sax
+from motionless import LatLonMarker, DecoratedMap
 from six.moves.urllib import request
+try:
+    import geojson
+except ImportError:
+    print('This example requires the geojson package')
+    import sys
+    sys.exit()
 
-class QuakeHandler(xml.sax.handler.ContentHandler):
-    
-    def __init__(self,gmap):
-        self.content = []
-        self.gmap = gmap
-        self.qname_point = ('http://www.georss.org/georss','point')
-        self.qname_title = ('http://www.w3.org/2005/Atom', 'title')
-        self.qname_entry = ('http://www.w3.org/2005/Atom', 'entry')
+# Get the geojson feed from USGS
+opener = request.urlopen('http://earthquake.usgs.gov/earthquakes/feed/v1.0'
+                         '/summary/2.5_day.geojson')
 
+# Parse it
+gjs = geojson.loads(opener.readall().decode('utf-8'))
 
-    def startElementNS(self, qname, name, attrs):
-        if qname in (self.qname_point,self.qname_title):
-            self.capture = True
-        else:
-            self.capture = False
-
-    def endElementNS(self, qname, name):
-        element = ''.join(self.content)
-        if qname == self.qname_point:
-            self.tokens = element.split()
-        if qname == self.qname_title:
-            element = ''.join(self.content)
-            magnitude = element[2]
-            if magnitude in ['2','3','4']:
-                self.color = 'yellow'
-                self.size = 'small'
-            if magnitude in ['5','6']:
-                self.color = 'orange'
-                self.size = 'small'
-            if magnitude in ['7','8','9']:
-                self.color = 'red'
-                self.size = 'mid'
-
-        if qname == self.qname_entry:
-            self.gmap.add_marker(LatLonMarker(self.tokens[0],self.tokens[1],color=self.color,size=self.size))
-
-        self.content = []
-
-    def characters(self, ch):
-        if self.capture:
-            self.content.append(ch)
-
-quake = DecoratedMap(size_x=640,size_y=440)
-parser = xml.sax.make_parser()
-parser.setContentHandler(QuakeHandler(quake))
-parser.setFeature(xml.sax.handler.feature_namespaces, 1)
-
-opener = request.urlopen( 'http://earthquake.usgs.gov/earthquakes/catalogs/1day-M2.5.xml')
-parser.feed(opener.read())
+# Prepare a map and add the points
+gmap = DecoratedMap(size_x=640,size_y=440)
+for feat in gjs.features:
+    magnitude = feat['properties']['mag']
+    lon, lat, _ = feat['geometry']["coordinates"]
+    if magnitude > 2:
+        color = 'yellow'
+        size = 'small'
+    if magnitude > 5:
+        color = 'orange'
+        size = 'small'
+    if magnitude > 7:
+        color = 'red'
+        size = 'mid'
+    gmap.add_marker(LatLonMarker(lat, lon, color=color, size=size))
 
 htmlPage = """
 <html>
 <body>
 <h2>Earthquakes 2.5+ today</h2>
-<i>GeoRSS feed parsed and visualized. Colors assigned to markers based on magnitued. Details can be found on <a href='http://earthquake.usgs.gov/earthquakes/catalogs/'>USGS web site</a>.
+<i>GeoRSS feed parsed and visualized. Colors assigned to markers based on
+magnitudes. Details can be found on the
+<a href='http://earthquake.usgs.gov/earthquakes/'>USGS web site</a>.
 <p/>
 <p/>
 <img src="%s"/>
 </body>
-</html>	
-""" % quake.generate_url()
-	 
+</html>
+""" % gmap.generate_url()
 
-html = open("earthquakes.html","w")
-html.write(htmlPage)
-html.close()
+with open("earthquakes.html","w")as html:
+    html.write(htmlPage)
 print("earthquakes.html file created")
